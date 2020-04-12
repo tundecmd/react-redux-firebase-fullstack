6e6b194758b0b558d44fab70b29d00b75d8b37bf
Adepoju Lateef <tundeadepoju22@gmail.com>
2020-04-12 07:51:41 +0100

    setting up the component architecture

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Yes</h1>
+      </div>
+    )
+  }
+}
+
+export default App
